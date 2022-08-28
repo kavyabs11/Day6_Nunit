12f9199023a89189175a07fb3f44986fa3c60d60
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-28 20:26:40 +0530

    [kavya] Add: TempCon

---
+++ Nunit/Nunit/TempCon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit
+{
+    public class TempCon
+    {
+        public static double Test(int ch, double convert_into)
+        {
+            double converted = 0;
+            if (ch == 1)
+            {
+                converted = (convert_into * 9 / 5) + 32;              //deg C to deg F
+            }
+            else
+            {
+                converted = (convert_into - 32) * 5 / 9;                  //deg F to deg C
+            }
+            return converted;
+        }
+    }
+}

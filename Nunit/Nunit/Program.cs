67318cfeb343d243bec5cfbfb5f719c8e90ee83e
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-28 20:31:02 +0530

    [kavya] Add: MOnthly Payment

--- Nunit/Nunit/Program.cs
+++ Nunit/Nunit/Program.cs
@@ -1,15 +1,9 @@
 ﻿using Nunit;
 Console.WriteLine("Welcome to NUnit Testing program");
-Console.WriteLine("Enter your choice : 1- Celsius to fahrenheit , 2- fahrenheit to Celsius");
-int choice = Convert.ToInt32(Console.ReadLine());
-
-if (choice != 1 && choice != 2)
-{
-    Console.WriteLine("Wrong input");
-}
-else
-{
-    Console.WriteLine("Enter the value you want to convert");
-    double convert = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine($"Converted value is = {TempCon.Test(choice, convert)}");
-}
+Console.WriteLine("Enter the principle amount");
+double P = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter for how many years");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the rate to be compounded monthly");
+double r = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Payment to be made monthly is = {MonthlyPay.Test(P, y, r)}");
